7b77a915932378dc19eb486a6fba598558fbe066
NeonMagique <noe.tritsch@epitech.eu>
2024-12-10 14:14:59 +0100

    feat: Day10

---
+++ Day10/Task2/Task2.js
@@ -0,0 +1,44 @@
+const fs = require('fs');
+const args = process.argv.slice(2);
+
+if (args.length < 1)
+    process.exit(84);
+
+function isValid(map, position)
+{
+    let [x, y] = position;
+    return y >= 0 && y < map.length && x >= 0 && x < map[y].length;
+}
+
+fs.readFile(args[0], 'utf-8', (err, data) => {
+    if (err)
+        process.exit(84);
+
+    const directions = [[0, -1], [1, 0], [0, 1], [-1, 0]];
+
+    const map = data.split('\n').filter(line => line.trim() !== '');
+    const base = map.flatMap((line, lineIndex) =>
+        [...line].map((char, charIndex) => char === '0' ? {x: charIndex, y: lineIndex} : null)
+    ).filter(Boolean);
+
+    let total = 0;
+    for (const {x, y} of base) {
+        let tmpPosition = [{ x: x, y: y, value: 0 }];
+
+        while (tmpPosition.length > 0) {
+            let {x: currentX, y: currentY, value} = tmpPosition.pop();
+            if (value === 9) {
+                total++;
+                continue;
+            }
+            for (const [offsetX, offsetY] of directions) {
+                let [tmpX, tmpY] = [currentX + offsetX, currentY + offsetY];
+                if (isValid(map, [tmpX, tmpY]) && map[tmpY][tmpX] == value + 1)
+                    tmpPosition.push({ x: tmpX, y: tmpY, value: value + 1 });
+            }
+        }
+    }
+    // * Print the Result
+    console.log("Count: ", total);
+    // * Answer should be 1034
+});
---
+++ Day10/execute.md
@@ -0,0 +1,4 @@
+# How to execute ?
+
+<!-- You need to install the icont binary from the official website -->
+icont Task
