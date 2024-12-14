a03583ef2ff51143f285ad91fd19ed602a25238b
NeonMagique <noe.tritsch@epitech.eu>
2024-12-14 17:44:01 +0100

    feat: update script for setup day

--- create_day.sh
+++ create_day.sh
@@ -1,19 +1,34 @@
 #!/bin/bash
 
-if [ -z "$1" ]; then
-  echo "Usage: $0 <day>"
+GREEN='\033[0;32m'
+RED='\033[0;31m'
+RESET='\033[0m'
+
+if [ -z "$1" ] || [ -z "$2" ]; then
+  echo -e "${RED}Usage: $0 <day> <extension>${RESET}"
   exit 1
 fi
 
 day="$1"
-mkdir -p "$day"
+extension="$2"
+
+echo -e "${GREEN}Creating directories for $day/Task1 and $day/Task2...${RESET}"
 mkdir -p "$day/Task1"
 mkdir -p "$day/Task2"
 
-input_file="$day/input.txt"
-touch "$input_file"
+echo -e "${GREEN}Creating Task1/Task1$extension and Task2/Task2$extension...${RESET}"
+touch "$day/Task1/Task1$extension"
+touch "$day/Task2/Task2$extension"
 
+echo -e "${GREEN}Creating input.txt in $day...${RESET}"
+input_file="$day/input.txt"
+day_number=$(echo "$day" | sed 's/[^0-9]*//g')
+token=$(grep -oP '^AOC_TOKEN=\K.*' .env)
+echo "Here is your token: ${token}"
+curl -s "https://adventofcode.com/2024/day/${day_number}/input" -H "cookie: session=${token}" -o "$input_file"
+
+echo -e "${GREEN}Creating and initializing execute.md...${RESET}"
 execute_file="$day/execute.md"
 echo "# How to execute ?" > "$execute_file"
 
-echo "Success"
+echo -e "${GREEN}Success: Files and directories for $day created.${RESET}"
